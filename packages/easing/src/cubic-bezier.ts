6bdcb91365e1488e39cbcd181736436d16c1ee0e
ERKELSOT <1256029807@qq.com>
2023-01-15 16:31:51 +0800

    feat: bezier curves docs

---
+++ packages/easing/src/cubic-bezier.ts
@@ -0,0 +1,21 @@
+/**
+ *  A Bézier curve (/ˈbɛz.i.eɪ/ BEH-zee-ay)[1] is a parametric curve used in computer graphics and related
+ *  fields.[2] A set of discrete "control points" defines a smooth, continuous curve by means of a formula.
+ *  Usually the curve is intended to approximate a real-world shape that otherwise has no mathematical representation or
+ *  whose representation is unknown or too complicated. The Bézier curve is named after French engineer Pierre Bézier (1910–1999),
+ *  who used it in the 1960s for designing curves for the bodywork of Renault cars.[3] Other uses include the design of computer fonts and animation.[3]
+ *  Bézier curves can be combined to form a Bézier spline,
+ *  or generalized to higher dimensions to form Bézier surfaces.[3]
+ *  The Bézier triangle is a special case of the latter.
+ */
+
+/**
+ * Inspiration Freya Holmér
+ * link https://www.youtube.com/watch?v=aVwxzDHniEw
+ * use online https://cubic-bezier.com/#.17,.67,.83,.67
+ */
+
+/**
+ *
+ *
+ */

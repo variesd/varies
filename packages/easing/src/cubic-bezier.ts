df50f10571bd72f1badd7fa054410c8c74b75403
ERKELSOT <1256029807@qq.com>
2023-01-24 14:21:46 +0800

    chore: easing packages

--- packages/easing/src/cubic-bezier.ts
+++ packages/easing/src/cubic-bezier.ts
@@ -19,4 +19,52 @@
  *
  *
  */
-export {}
+// Returns x(t) given t, x1, and x2, or y(t) given t, y1, and y2.
+const calcBezier = (t: number, a1: number, a2: number) =>
+  (((1.0 - 3.0 * a2 + 3.0 * a1) * t + (3.0 * a2 - 6.0 * a1)) * t + 3.0 * a1) * t
+
+const subdivisionPrecision = 0.0000001
+const subdivisionMaxIterations = 12
+export const noopReturn = <V>(v: V) => v
+function binarySubdivide(
+  x: number,
+  lowerBound: number,
+  upperBound: number,
+  mX1: number,
+  mX2: number
+) {
+  let currentX: number
+  let currentT: number
+  let i: number = 0
+
+  do {
+    currentT = lowerBound + (upperBound - lowerBound) / 2.0
+    currentX = calcBezier(currentT, mX1, mX2) - x
+    if (currentX > 0.0) {
+      upperBound = currentT
+    } else {
+      lowerBound = currentT
+    }
+  } while (
+    Math.abs(currentX) > subdivisionPrecision &&
+    ++i < subdivisionMaxIterations
+  )
+
+  return currentT
+}
+
+export function cubicBezier(
+  mX1: number,
+  mY1: number,
+  mX2: number,
+  mY2: number
+) {
+  // If this is a linear gradient, return linear easing
+  if (mX1 === mY1 && mX2 === mY2) return noopReturn
+
+  const getTForX = (aX: number) => binarySubdivide(aX, 0, 1, mX1, mX2)
+
+  // If animation is at start/end, return t without easing
+  return (t: number) =>
+    t === 0 || t === 1 ? t : calcBezier(getTForX(t), mY1, mY2)
+}
